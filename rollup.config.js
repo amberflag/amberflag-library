444f534c0ef641caa045a6e36914048634ce688d
Jose Manuel Rodriguez Calvo <jmr@MacBook-Pro-de-Jose.local>
2024-04-21 17:58:33 +0200

    remove bun and try with npm

--- rollup.config.js
+++ rollup.config.js
@@ -7,9 +7,6 @@ const packageJson = require('./package.json')
 export default {
   input: 'src/main.ts',
   output: [{ file: packageJson.main , format: 'cjs', sourceMap: true , }, { file: packageJson.module , format: 'esm', sourceMap: true }],
-  // sourceMap: true,
-  // sourceMapFile: 'dist/bundle.js.map',
-
   plugins: [
     peerDepsExternal(),
     commonjs(),
